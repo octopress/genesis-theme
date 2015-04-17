9264b474a78470136b5d3000848efc83f94addba
Brandon Mathis <brandon@imathis.com>
2015-04-16 23:18:30 -0500

    Footnotes look good

--- assets/javascripts/littlefoot.js
+++ assets/javascripts/littlefoot.js
@@ -1,7 +1,7 @@
 (function(){
 
-  littleFoot = {
-    setup: function(){
+  var LittleFoot = {
+    initialize: function(){
       self = this
 
       document.addEventListener("DOMContentLoaded", function(event) { 
@@ -13,38 +13,118 @@
         document.querySelector('div.footnotes').remove()
       })
 
+      window.addEventListener("resize", function(event) { 
+        window.clearTimeout(self.timeout)
+        self.timeout = window.setTimeout(function(){self.positionFootnote()}, 50)
+      })
+
     },
 
-    toggleFootnote: function(event) {
+    toggleFootnote: function (event) {
+      self = this
       el = event.target
-      if (el.classList.toggle('is-active')){
-        el.insertAdjacentHTML('afterend', el.footnote);
+      el.parentNode.classList.toggle('is-open')
+
+      var footnote = el.nextElementSibling
+      var size = this.measureFootnote(footnote)
+
+      if (!footnote.classList.toggle('is-hidden')) {
+        this.currentFootnote = footnote
       } else {
-        el.nextElementSibling.remove()
+        this.currentFootnote = null
       }
+
+      footnote.querySelector('.littlefoot-footnote-content').setAttribute('style', 'max-width:'+size.w+'px;')
+      window.setTimeout(function(){self.positionFootnote()}, 10)
+      footnote.classList.toggle('is-visible')
     },
     
-    setupFootnote: function(el) {
-      var sup = el.parentNode
-      var id = el.getAttribute('href').replace(':', "\\:")
-      var footnote = document.querySelector(id).cloneNode(true)
-      footnote.querySelector('a[href^=\\#fnref]').remove()
-      var noteContent = footnote.innerHTML.trim()
-      sup.insertAdjacentHTML('afterend', this.button(el.textContent));
-      var button = sup.nextElementSibling
-      button.footnote = this.footnote(noteContent)
-      button.addEventListener('click', this.toggleFootnote.bind(this))
+    setupFootnote: function (anchor) {
+      // We want to add the footnote adjacent to the footnote <sup> element
+      var sup = anchor.parentNode
+      sup.insertAdjacentHTML('afterend', this.wrapper(anchor));
+
+      var wrapper = sup.nextElementSibling
+      wrapper.footnote = this.footnoteContent(anchor)
+      wrapper.querySelector('button').addEventListener('click', this.toggleFootnote.bind(this))
       sup.remove()
     },
-    
-    button: function(content) {
-      return '<button class="littlefoot-button">'+content+'</button>'
+
+    footnoteContent: function(anchor) {
+
+      // Find the footnote from the id in the anchor's href
+      var id = anchor.getAttribute('href').replace(':', "\\:")
+      var footnote = document.querySelector(id)
+
+      // Remove the "return to content" link
+      var link = footnote.querySelector('a[href^=\\#fnref]')
+      if (link) link.remove()
+      var containerWidth = footnote.offsetParent.parentElement.offsetWidth
+      return footnote.innerHTML
+    },
+
+    wrapper: function (el) {
+      return ' <div class="littlefoot-container open-down">'+this.button(el)+this.footnote(el)+'</div>'
+    },
+
+    button: function (el) {
+      return '<button class="littlefoot-button" title="view footnote #'+el.textContent+'">•••</button>'
     },
     
-    footnote: function(content) {
-      return '<aside class="littlefoot-footnote">'+content+'</aside>'
+    footnote: function (el) {
+      return '<aside class="littlefoot-footnote is-hidden"><div class="littlefoot-footnote-wrapper"><div class="littlefoot-footnote-content">'+this.footnoteContent(el)+'</div></div></aside>'
+    },
+
+    positionFootnote: function () {
+      if (this.currentFootnote && this.currentFootnote.offsetWidth > 0) {
+        this.setOrientation(this.currentFootnote)
+        this.setPosition(this.currentFootnote)
+      }
+    },
+
+    setPosition: function(footnote) {
+      var container = footnote.offsetParent.parentElement
+      var contentWidth = container.offsetWidth
+      var overlap =  footnote.offsetWidth + footnote.offsetParent.offsetLeft
+
+      footnote.setAttribute('style', '')
+
+      if (overlap > contentWidth) {
+        footnote.setAttribute('style', 'left: -' + String(overlap - contentWidth) + 'px;')
+      }
+    },
+
+    setOrientation: function (footnote) {
+      var wrapper = footnote.parentElement
+      wrapper.classList.remove('open-up', 'open-down')
+      wrapper.classList.add('open-down')
+      var pos = footnote.getBoundingClientRect()
+
+      wrapper.classList.remove('open-up', 'open-down')
+
+
+      if (pos.bottom > window.innerHeight) {
+        wrapper.classList.add('open-up')
+      } else {
+        wrapper.classList.add('open-down')
+      }
+    },
+
+    measureFootnote: function(footnote) {
+      var context = footnote.parentElement.parentElement
+      var el = document.createElement("div")
+      el.classList.add('littlefoot-test')
+      el.insertAdjacentHTML('beforeend', footnote.parentElement.footnote)
+      context.appendChild(el)
+      var size = {
+        h: el.offsetHeight,
+        w: el.offsetWidth
+      }
+      context.removeChild(el)
+      return size
     }
   }
 
-  littleFoot.setup()
+  LittleFoot.initialize()
+  window.LittleFoot = LittleFoot
 })()
--- demo/footnotes.md
+++ demo/footnotes.md
@@ -3,7 +3,7 @@ layout: theme:page
 title: test footnotes
 ---
 
-some awesome page[^1] about whatever
+Some awesome there are so many things about this page [^1] but to name the most important, I'll have to say it's your mom.
 
 
-[^1]: Some footnote
+[^1]: Some footnote which is really long and is pretty much a whole paragraph. I wonder how this will look. Surely it will wrap.Some footnote which is really long and is pretty much a whole paragraph. I wonder how this will look. Surely it will wrap.
